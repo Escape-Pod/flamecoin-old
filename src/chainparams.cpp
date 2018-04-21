ed04a3343e57901817476f47b2497c14a7b72ee9
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-04-21 22:30:25 +1200

    Fix blockReward

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -202,7 +202,7 @@ public:
         nDefaultPort = 19335;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1486949366, 2611779, 0x1e0ffff0, 1, 1000 * COIN);
+        genesis = CreateGenesisBlock(1486949366, 2611779, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x8a55b44b1ec2b0e9467331fdabec0f552ebe2fad3f9b65ae3e79a05ad5b5bcd2"));
         assert(genesis.hashMerkleRoot == uint256S("0x99652d048027ac252d71ed5f18fa0cc0badaa2e6e08d66309ec2de89109804c5"));
@@ -283,7 +283,7 @@ public:
         nDefaultPort = 19444;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1296688602, 0, 0x207fffff, 1, 1000 * COIN);
+        genesis = CreateGenesisBlock(1296688602, 0, 0x207fffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
         assert(consensus.hashGenesisBlock == uint256S("0x530827f38f93b43ed12af0b3ad25a288dc02ed74d6d7857862df51fc56c416f9"));
         assert(genesis.hashMerkleRoot == uint256S("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
